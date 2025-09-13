6a200acc8a9f75cb7bbb2d95c78ff4006628cc34
g-test2025 <gerald_nelson007@hotmail.com>
2025-09-13 19:13:51 +0000

    This Chnages for GX Events

--- frontend/src/components/common/PoweredByFooter/index.tsx
+++ frontend/src/components/common/PoweredByFooter/index.tsx
@@ -1,8 +1,11 @@
-import {t} from "@lingui/macro";
+import { t } from "@lingui/macro";
 import classes from "./FloatingPoweredBy.module.scss";
 import classNames from "classnames";
 import React from "react";
-import {iHavePurchasedALicence, isHiEvents} from "../../../utilites/helpers.ts";
+import {
+  iHavePurchasedALicence,
+  isHiEvents,
+} from "../../../utilites/helpers.ts";
 
 /**
  * (c) Hi.Events Ltd 2025
@@ -17,37 +20,49 @@ import {iHavePurchasedALicence, isHiEvents} from "../../../utilites/helpers.ts";
  *
  * If you wish to remove this notice, a commercial license is available at: https://hi.events/licensing
  */
-export const PoweredByFooter = (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => {
-    if (iHavePurchasedALicence()) {
-        return <></>;
-    }
+export const PoweredByFooter = (
+  props: React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >
+) => {
+  if (iHavePurchasedALicence()) {
+    return <></>;
+  }
 
-    const footerContent = isHiEvents() ? (
-        <>
-            {t`Planning an event?`} {' '}
-            <a href="https://hi.events?utm_source=app-powered-by-footer&utm_content=try-hi-events-free"
-               target="_blank"
-               className={classes.ctaLink}
-               title={'Effortlessly manage events and sell tickets online with Hi.Events'}>
-                {t`Try Hi.Events Free`}
-            </a>
-        </>
-    ) : (
-        <>
-            {t`Powered by`} {' '}
-            <a href="https://hi.events?utm_source=app-powered-by-footer"
-               target="_blank"
-               title={'Effortlessly manage events and sell tickets online with Hi.Events'}>
-                Hi.Events
-            </a> 🚀
-        </>
-    );
+  const footerContent = isHiEvents() ? (
+    <>
+      {t`Planning an event?`}{" "}
+      <a
+        href="https://www.g-techthailand.com"
+        target="_blank"
+        className={classes.ctaLink}
+        title={
+          "Effortlessly manage events and sell tickets online with GX Events"
+        }
+      >
+        {t`Try Hi.Events Free`}
+      </a>
+    </>
+  ) : (
+    <>
+      {t`Powered by`}{" "}
+      <a
+        href="https://www.g-techthailand.com"
+        target="_blank"
+        title={
+          "Effortlessly manage events and sell tickets online with GX Events"
+        }
+      >
+        GX.Events
+      </a>{" "}
+      🚀
+    </>
+  );
 
-    return (
-        <div {...props} className={classNames(classes.poweredBy, props.className)}>
-            <div className={classes.poweredByText}>
-                {footerContent}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div {...props} className={classNames(classes.poweredBy, props.className)}>
+      <div className={classes.poweredByText}>{footerContent}</div>
+    </div>
+  );
+};
